2dd9b9b7170da9995be9f997d4a3e2cfd304aa57
anass17 <anassboutaib2018@gmail.com>
2025-12-18 14:47:10 +0100

    AA-46 Nettoyer et prétraiter les nouveaux avis

--- client/get_microbatch.py
+++ client/get_microbatch.py
@@ -1,8 +1,21 @@
 import requests
+import re
 
 url = "http://127.0.0.1:8000/api/batch"
 
 response = requests.get(url)
 
-print(response.status_code)
-print(response.json())
+if response.status_code != 200:
+    print("Error! Could not load data from the API")
+    exit()
+
+data = response.json()
+
+### Nettoyer et prétraiter les nouveaux avis
+
+for item in data:
+    item['text'] = re.sub(r'@[^\s]+', '', item['text'])
+    item['text'] = re.sub(r'\s+', ' ', item['text'])
+    item['text'] = item['text'].strip()
+
+print(data)
